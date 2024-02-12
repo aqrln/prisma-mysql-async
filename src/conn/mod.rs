140f2430b5faa535397aaee960e347cb867a1d0a
Flavian Desverne <flavian.dsv@gmail.com>
2024-02-12 18:47:31 +0100

    fix err

--- src/conn/mod.rs
+++ src/conn/mod.rs
@@ -505,9 +505,9 @@ impl Conn {
         FIXED_MARIADB_VERSION_RE.captures(version).map(|captures| {
             // Should not panic because validated with regex
             (
-                parse::<u16, _>(captures.get(1).unwrap().as_bytes()).unwrap(),
-                parse::<u16, _>(captures.get(2).unwrap().as_bytes()).unwrap(),
-                parse::<u16, _>(captures.get(3).unwrap().as_bytes()).unwrap(),
+                captures.get(1).unwrap().as_str().parse().unwrap(),
+                captures.get(2).unwrap().as_str().parse().unwrap(),
+                captures.get(3).unwrap().as_str().parse().unwrap(),
             )
         })
     }
